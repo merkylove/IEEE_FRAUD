699496fddbc0497443dcf3108c280fd3e27f97ed
Roman Merkulov <r_merkulov@indatalabs.com>
2019-09-07 00:08:59 +0300

    fixes

--- generate_features.py
+++ generate_features.py
@@ -7,7 +7,7 @@ from feature_engineering import add_datetime_features, process_id_30, \
 from settings import CATEGORICAL_FEATURES, TARGET
 
 
-def generate_features(train, test, mode='train'):
+def generate_features_time_series(train, test, bounds=(12, 13, 14, 15, 16)):
 
     '''
 
@@ -16,8 +16,6 @@ def generate_features(train, test, mode='train'):
     :param mode: train or inference
     :return:
     '''
-
-    bound = 16 if mode == 'train' else 17
 
     train_size = train.shape[0]
     test_size = test.shape[0]
@@ -36,6 +34,7 @@ def generate_features(train, test, mode='train'):
             ['card4'],
             ['card5'],
             ['card6'],
+            ['card1', 'card2', 'card3', 'card4'],
             ['addr1'],
             ['addr2'],
             ['TransactionDT_hour'],
@@ -47,17 +46,7 @@ def generate_features(train, test, mode='train'):
             # derived
             ['OS_NAME'],
             ['id_30'],
-            ['TransactionDT_dayOfWeek', 'addr1'],
-            ['TransactionDT_weekOfMonth', 'addr1'],
-            ['M1'],
-            ['M2'],
-            ['M3'],
-            ['M4'],
-            ['M5'],
-            ['M6'],
-            ['M7'],
-            ['M8'],
-            ['M9']
+            ['ProductCD']
         ]
     )
 
@@ -65,6 +54,13 @@ def generate_features(train, test, mode='train'):
     train_test_joined = smoothed_encodings(
         train_test_joined,
         [
+            ['card1'],
+            ['card2'],
+            ['card3'],
+            ['card4'],
+            ['card5'],
+            ['card6'],
+            ['card1', 'card2', 'card3', 'card4'],
             ['addr1'],
             ['addr2'],
             ['TransactionDT_hour'],
@@ -76,18 +72,26 @@ def generate_features(train, test, mode='train'):
             # derived
             ['OS_NAME'],
             ['id_30'],
+            ['ProductCD']
         ],
         TARGET,
-        funcs=['mean', 'std'],
-        train_size=sum(train_test_joined['TransactionDT_split'] <= bound)
+        funcs=['mean'],
+        train_size=sum(train_test_joined['TransactionDT_split'].isin(bounds))
     )
 
     # mean, std encodings
-    for val in ['TransactionAmt', 'id_02', 'D15', 'C13']:
+    for val in ['TransactionAmt', 'id_02']:
 
         train_test_joined = smoothed_encodings(
             train_test_joined,
             [
+                ['card1'],
+                ['card2'],
+                ['card3'],
+                ['card4'],
+                ['card5'],
+                ['card6'],
+                ['card1', 'card2', 'card3', 'card4'],
                 ['addr1'],
                 ['addr2'],
                 ['TransactionDT_hour'],
@@ -99,11 +103,12 @@ def generate_features(train, test, mode='train'):
                 # derived
                 ['OS_NAME'],
                 ['id_30'],
+                ['ProductCD']
             ],
             val
         )
 
-    train_test_joined = V_features_to_PCA(train_test_joined)
+    #train_test_joined = V_features_to_PCA(train_test_joined)
 
     train_test_joined, encoders = encode_categorical_features(
         train_test_joined,
