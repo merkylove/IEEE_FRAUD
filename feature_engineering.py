fc0bff4971a6c61964f2b2374006beca270208ff
EC2 Default User <ec2-user@ip-10-9-0-6.ec2.internal>
2019-09-08 22:04:39 +0000

    ...

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,12 +1,33 @@
 import numpy as np
 import pandas as pd
+from pandas.tseries.holiday import USFederalHolidayCalendar as calendar
 import datetime
 
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-from settings import START_DATE
+from settings import START_DATE, V_GROUPS_BY_NOTNULL
+
+
+def generate_uid_features(df):
+
+    df['bank_type'] = df['card3'].astype(str) + '_' + df['card5'].astype(str)
+    df['uid'] = df['card1'].astype(str) + '_' + df['card2'].astype(str)
+    df['uid2'] = df['uid'].astype(str) + '_' + df['card3'].astype(str) + '_' + df['card5'].astype(str)
+    df['uid3'] = df['uid2'].astype(str) + '_' + df['addr1'].astype(str) + '_' + df['addr2'].astype(str)
+    df['uid4'] = df['uid3'].astype(str) + '_' + df['P_emaildomain'].astype(str)
+    df['uid5'] = df['uid3'].astype(str) + '_' + df['R_emaildomain'].astype(str)
+
+    return df
+
+
+def exchange_rate_took_place_feature(df):
+    df['is_foreign'] = df['TransactionAmt'].astype('str')\
+        .str\
+        .split('.', expand=True)[1]\
+        .apply(lambda x: len(x) > 2)
+    return df
 
 
 def calc_smooth_mean(
@@ -14,7 +35,7 @@ def calc_smooth_mean(
         columns_agg,
         funcs=['mean', 'std', np.nanmedian],
         on='isFraud',
-        m=100,
+        m=50,
         to_round=5
 ):
 
@@ -57,7 +78,8 @@ def smoothed_encodings(
         columns_agg,
         column_value,
         funcs=['mean', 'std', np.nanmedian],
-        train_size=None
+        train_size=None,
+        m=50
 ):
     if train_size is None:
         train_size = df.shape[0]
@@ -67,7 +89,8 @@ def smoothed_encodings(
             df.iloc[:int(train_size)],
             col_agg,
             funcs=funcs,
-            on=column_value
+            on=column_value,
+            m=m
         )
 
         for f in funcs:
@@ -91,7 +114,8 @@ def smoothed_encodings(
 
 def count_features(
         df,
-        columns_agg
+        columns_agg,
+        with_typical_for_user=True
 ):
     for c_agg in columns_agg:
 
@@ -100,6 +124,10 @@ def count_features(
         df[f'{col_name}_count'] = df[c_agg]\
             .groupby(c_agg)[c_agg[0]]\
             .transform('count')
+
+        if with_typical_for_user and len(c_agg) > 1:
+            df[f'{col_name}_count_how_typical'] = df[f'{col_name}_count'] / \
+                df.groupby(['card1'])['TransactionDT'].transform('count')
 
     return df
 
@@ -132,13 +160,28 @@ def add_datetime_features(df):
     df[f'{tr_dt}_month'] = df[f'{tr_dt}_to_datetime'].dt.month
     df[f'{tr_dt}_dayOfMonth'] = df[f'{tr_dt}_to_datetime'].dt.day
     df[f'{tr_dt}_dayOfWeek'] = df[f'{tr_dt}_to_datetime'].apply(
-        lambda x: x.weekday())
+        lambda x: x.weekday()
+    )
     df[f'{tr_dt}_weekOfMonth'] = (df[f'{tr_dt}_to_datetime'].dt.day - 1) // 7 + 1
     df[f'{tr_dt}_hour'] = df[f'{tr_dt}_to_datetime'].dt.hour
     #df[f'{tr_dt}_minute'] = df[f'{tr_dt}_to_datetime'].dt.minute
     #df[f'{tr_dt}_second'] = df[f'{tr_dt}_to_datetime'].dt.second
     df[f'{tr_dt}_split'] = (df[f'{tr_dt}_to_datetime'].dt.year - 2017) * 12 + \
                                 df[f'{tr_dt}_to_datetime'].dt.month
+
+
+    # US HOLIDAYS
+    dates_range = pd.date_range(start='2017-10-01', end='2019-01-01')
+    us_holidays = calendar().holidays(
+        start=dates_range.min(),
+        end=dates_range.max()
+    )
+    df['is_holiday'] = df[f'{tr_dt}_to_datetime']\
+        .dt\
+        .date\
+        .astype('datetime64')\
+        .isin(us_holidays)\
+        .astype(np.int8)
 
     df.drop(
         labels=[f'{tr_dt}_year', f'{tr_dt}_to_datetime', f'{tr_dt}_month'],
@@ -215,18 +258,18 @@ def process_id_30(df):
     df['OS_V_COMBINED'] = df['OS_V0'] * 1000 + df['OS_V1'] * 10 + df['OS_V2'].fillna(0)
     df['OS_V_COMBINED'] = df['OS_V_COMBINED'].astype(float)
 
+    df['OS_V_MAJOR'] = df['OS_NAME'].astype(str) + '_' + df['OS_V0'].astype(str)
+
     return df
 
 
-def V_features_to_PCA(df):
+def features_to_PCA(df, columns, prefix, n_components=0.95):
 
-    pca_reducer = PCA(n_components=0.85)
-
-    V_features = [f'V{i}' for i in range(1, 340)]
+    pca_reducer = PCA(n_components=n_components)
 
     transformed_values = pca_reducer.fit_transform(
         StandardScaler().fit_transform(
-            df[V_features].fillna(0.0)
+            df[columns].fillna(0.0)
         )
     )
 
@@ -234,12 +277,37 @@ def V_features_to_PCA(df):
 
     pca_df = pd.DataFrame(
         data=transformed_values,
-        columns=[f'PCA_V{i}' for i in range(size)],
+        columns=[f'PCA_{prefix}{i}' for i in range(size)],
         index=df.index
     )
 
-    df.drop(labels=V_features, axis=1, inplace=True)
+    #df.drop(labels=columns, axis=1, inplace=True)
 
     df = pd.concat([df, pca_df], axis=1)
 
     return df
+
+
+def V_features_to_PCA(df):
+
+    for k, v in V_GROUPS_BY_NOTNULL.items():
+        df = features_to_PCA(df, v, prefix=f'V_{k}_GROUP_')
+
+    return df
+
+
+def C_features_to_PCA(df):
+    return features_to_PCA(df, [f'C{i}' for i in range(1, 15)], prefix='C')
+
+
+def D_features_to_PCA(df):
+    return features_to_PCA(df, [f'D{i}' for i in range(1, 16)], prefix='D')
+
+
+def log_features(df, colums):
+    df[colums] = np.log(df[colums] + 1.0)
+    return df
+
+
+def C_log_features(df):
+    return log_features(df, [f'C{i}' for i in range(1, 15)])
